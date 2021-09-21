ea69682726234665025b1ae4dcd2872277762b83
Steven Black <steven@springsurprise.com>
2021-09-21 10:51:14 -0700

    - Added comments and instructions regarding key initialization

--- purpleair_test.go
+++ purpleair_test.go
@@ -7,8 +7,13 @@ import (
 	"testing"
 )
 
+// Key map for holding the read & write keys for API access
+// Initialized by reading in local JSON file "keys.JSON"
+// Keys are available by requesting from "contact@purpleair.com"
 var km map[string]string
 
+// Initialization of read & write keys used for API access
+// If keys are not available, then unable to perform any API tests
 func init() {
 	f, err := ioutil.ReadFile("./keys.JSON")
 	if err != nil {
@@ -23,7 +28,9 @@ func init() {
 	log.Printf("Keys initialized\n")
 }
 
+// Suite of tests related to key validation
 func TestKeys(t *testing.T) {
+	// read key check
 	kt, err := CheckAPIKey(km["read"])
 	if err != nil {
 		t.Log("Unable to CheckAPIKey", err)
@@ -33,6 +40,8 @@ func TestKeys(t *testing.T) {
 		t.Log("Expected read key, got", kt)
 		t.Fail()
 	}
+
+	// write key check
 	kt, err = CheckAPIKey(km["write"])
 	if err != nil {
 		t.Log("Unable to CheckAPIKey", err)
@@ -42,6 +51,8 @@ func TestKeys(t *testing.T) {
 		t.Log("Expected write key, got", kt)
 		t.Fail()
 	}
+
+	// bogus key check
 	kt, err = CheckAPIKey("BOGUS")
 	if err == nil {
 		t.Log("Missing error for bogus key on CheckAPIKey")
