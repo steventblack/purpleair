2f0faa2a9aa45066fa5dad8672a2f8e8ecaa8b7c
Steven Black <steven@springsurprise.com>
2021-09-21 10:35:02 -0700

    purpleair_test.go

---
+++ purpleair_test.go
@@ -0,0 +1,59 @@
+package purpleair
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"testing"
+)
+
+var km map[string]string
+
+func init() {
+	f, err := ioutil.ReadFile("./keys.JSON")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(f, &km)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Keys initialized\n")
+}
+
+func TestKeys(t *testing.T) {
+	kt, err := CheckAPIKey(km["read"])
+	if err != nil {
+		t.Log("Unable to CheckAPIKey", err)
+		t.Fail()
+	}
+	if kt != APIKEYREAD {
+		t.Log("Expected read key, got", kt)
+		t.Fail()
+	}
+	kt, err = CheckAPIKey(km["write"])
+	if err != nil {
+		t.Log("Unable to CheckAPIKey", err)
+		t.Fail()
+	}
+	if kt != APIKEYWRITE {
+		t.Log("Expected write key, got", kt)
+		t.Fail()
+	}
+	kt, err = CheckAPIKey("BOGUS")
+	if err != nil {
+		t.Log("Unable to CheckAPIKey", err)
+	}
+	if kt != APIKEYUNKNOWN {
+		t.Log("Expected unknown key, got", kt)
+		t.Fail()
+	}
+}
+
+func TestGroups(t *testing.T) {
+}
+
+func TestSensorInfo(t *testing.T) {
+}
