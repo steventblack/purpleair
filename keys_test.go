b21025c6c5f8ba1cd5386293c752ed3d6daf8973
Steven Black <steven@springsurprise.com>
2021-10-31 12:36:08 -0700

    Relocated keys-related testing to keys_test.go - removed section from purpleair_test.go

---
+++ keys_test.go
@@ -0,0 +1,84 @@
+package purpleair
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"testing"
+)
+
+func init() {
+	f, err := ioutil.ReadFile("./keys.JSON")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(f, &ti)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestCheckAPIKey(t *testing.T) {
+	kt, err := CheckAPIKey(ti.Keys["read"])
+	if err != nil {
+		t.Log(t.Name(), err)
+		t.Fail()
+	}
+	if kt != KeyRead {
+		t.Logf("%s: Expected %s, got %s\n", t.Name(), KeyRead, kt)
+		t.Fail()
+	}
+
+	kt, err = CheckAPIKey(ti.Keys["write"])
+	if err != nil {
+		t.Log(t.Name(), err)
+		t.Fail()
+	}
+	if kt != KeyWrite {
+		t.Logf("%s: Expected %s, got %s\n", t.Name(), KeyWrite, kt)
+		t.Fail()
+	}
+
+	kt, err = CheckAPIKey("BOGUS")
+	if err == nil {
+		t.Logf("%s: Expected error, got nil\n", t.Name())
+		t.Fail()
+	}
+	if kt != KeyUnknown {
+		t.Logf("%s: Expected %s, got %s\n", t.Name(), KeyUnknown, kt)
+		t.Fail()
+	}
+}
+
+func TestSetAPIKey(t *testing.T) {
+	kt, err := SetAPIKey(ti.Keys["read"])
+	if err != nil {
+		t.Log(t.Name(), err)
+		t.Fail()
+	}
+	if kt != KeyRead {
+		t.Logf("%s: Expected %s, got %s\n", t.Name(), KeyRead, kt)
+		t.Fail()
+	}
+
+	kt, err = SetAPIKey(ti.Keys["write"])
+	if err != nil {
+		t.Log(t.Name(), err)
+		t.Fail()
+	}
+	if kt != KeyWrite {
+		t.Logf("%s: Expected %s, got %s\n", t.Name(), KeyWrite, kt)
+		t.Fail()
+	}
+
+	kt, err = SetAPIKey("BOGUS")
+	if err == nil {
+		t.Logf("%s: Expected error, got nil\n", t.Name())
+		t.Fail()
+	}
+	if kt != KeyUnknown {
+		t.Logf("%s: Expected %s, got %s\n", t.Name(), KeyUnknown, kt)
+		t.Fail()
+	}
+}
